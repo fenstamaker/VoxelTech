40f3af85cb79b8f3045111601442fa4d091b1a57
Fenstamaker Gary <fenstamaker@gmail.com>
2011-11-07 11:29:48 -0500

    Adding world files

---
+++ src/org/voxeltech/game/World.java
@@ -0,0 +1,16 @@
+package org.voxeltech.game;
+
+import java.util.ArrayList;
+import org.lwjgl.util.vector.Vector3f;
+
+import org.voxeltech.graphics.*;
+import org.voxeltech.noise.*;
+import org.voxeltech.utils.*;
+
+public class World {
+
+	private ProgramClock clock = ProgramClock.getInstance();
+	private ArrayList< ArrayList< ArrayList<Voxel> > > voxels;
+	
+	
+}
