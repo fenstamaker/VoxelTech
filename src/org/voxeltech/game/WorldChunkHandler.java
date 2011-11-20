6b158b88438a428f466559bee595839bd2e3519f
Fenstamaker Gary <fenstamaker@gmail.com>
2011-11-20 01:15:50 -0500

    Implemented sqlite. Memory leak causes heap out of memory error. Chunks and voxels need to become smaller

--- src/org/voxeltech/game/WorldChunkHandler.java
+++ src/org/voxeltech/game/WorldChunkHandler.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class WorldChunkHandler {
 
-	private ArrayList<WorldChunk> chunks;
+	private ArrayList<Integer[]> chunks;
 	private Connection connection;
 
 	private ObjectOutputStream objectOut;
@@ -16,7 +16,7 @@ public class WorldChunkHandler {
 	private ByteArrayInputStream bufferIn;
 	
 	public WorldChunkHandler() {
-		chunks = new ArrayList<WorldChunk>();
+		chunks = new ArrayList<Integer[]>();
 		
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -35,10 +35,9 @@ public class WorldChunkHandler {
 		return new WorldChunk(x, y, z);
 	}
 	
-	public WorldChunk loadChunk(int x, int y, int z) {
-		
-		WorldChunk chunk = null;
-		int id = x+y+z;
+	public ArrayList<WorldChunk> loadChunk(int x, int y, int z) {
+		ArrayList<WorldChunk> chunkHolder = new ArrayList<WorldChunk>();
+		ArrayList<Integer> foundChunks = new ArrayList<Integer>();
 		
 		try {
 			
@@ -46,30 +45,58 @@ public class WorldChunkHandler {
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(15);
 			
-			statement.executeUpdate("CREATE TABLE IF NOT EXISTS chunks (id int, data BLOB)");
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS chunks (id VARCHAR(15), data BLOB)");
 			
 			PreparedStatement prepStatement;
-			ResultSet rs = statement.executeQuery("SELECT * FROM chunks WHERE id=" + id + " LIMIT 1");
 			
-			if(!rs.next()) {
-				chunk = generateChunk(x, y, z);
-				bufferOut = new ByteArrayOutputStream();
-				objectOut= new ObjectOutputStream(bufferOut);
-				objectOut.writeObject(chunk);
-				objectOut.close();
+			String sql = "SELECT * FROM chunks WHERE id IN (";
+			
+			for(int i = 0; i < chunks.size(); i++) {
+				if(i!=0)
+					sql += ",";
+				String nameString = chunks.get(i)[0].toString() + chunks.get(i)[1].toString() + chunks.get(i)[2].toString();
+				sql += nameString;
+			}
+			
+			sql += ")";
+			
+			ResultSet rs = statement.executeQuery(sql);
+			
+			while(rs.next()) {
+				foundChunks.add(rs.getInt("id"));
 				
-				prepStatement = connection.prepareStatement("INSERT INTO chunks VALUES (?, ?)");
-				prepStatement.setInt(1, id);
-				prepStatement.setBytes(2, bufferOut.toByteArray());
-				prepStatement.execute();
-				bufferOut.close();
-			} else {
 				byte[] byteBuffer = rs.getBytes("data");
+				
 				bufferIn = new ByteArrayInputStream(byteBuffer);
 				objectIn = new ObjectInputStream(bufferIn);
-				chunk = (WorldChunk)objectIn.readObject();
+				
+				chunkHolder.add( (WorldChunk)objectIn.readObject() );
+				
 				objectIn.close();
 				bufferIn.close();
+			}
+			
+			WorldChunk tempChunk;
+			
+			for(Integer[] i : chunks) {
+				
+				if(!foundChunks.contains(i)) {
+					
+					tempChunk = generateChunk(x, y, z);
+					bufferOut = new ByteArrayOutputStream();
+					objectOut= new ObjectOutputStream(bufferOut);
+					objectOut.writeObject(tempChunk);
+					objectOut.close();
+					
+					prepStatement = connection.prepareStatement("INSERT INTO chunks VALUES (?, ?)");
+					String nameString = i[0].toString() + i[1].toString() + i[2].toString();
+					prepStatement.setString(1, nameString);
+					prepStatement.setBytes(2, bufferOut.toByteArray());
+					prepStatement.execute();
+					bufferOut.close();
+					
+				}
+				
 			}
 			
 			rs.close();
@@ -81,7 +108,7 @@ public class WorldChunkHandler {
 			System.exit(1);
 		}
 		
-		return chunk;
+		return chunkHolder;
 		/*
 		for(WorldChunk c : chunks) {
 			if(c.coordinates[0] == x && c.coordinates[1] == y && c.coordinates[2] == z)
@@ -93,24 +120,24 @@ public class WorldChunkHandler {
 	
 	
 	public ArrayList<WorldChunk> loadChunksAroundChunk(int x, int y, int z) {
-		ArrayList<WorldChunk> chunkHolder = new ArrayList<WorldChunk>();
+		
 		
 		for(int i = 0; i < 2; i++) {
 			for(int j = 0; j < 2; j++) {
 				for(int k = 0; k < 2; k++) {
-					chunkHolder.add(loadChunk(x+i, y+j, z+k));
-					chunkHolder.add(loadChunk(x+i, y+j, z-k));
-					chunkHolder.add(loadChunk(x+i, y-j, z+k));
-					chunkHolder.add(loadChunk(x+i, y-j, z-k));
-					chunkHolder.add(loadChunk(x-i, y+j, z+k));
-					chunkHolder.add(loadChunk(x-i, y+j, z-k));
-					chunkHolder.add(loadChunk(x-i, y-j, z+k));
-					chunkHolder.add(loadChunk(x-i, y-j, z-k));
+					chunks.add( new Integer[] { (x+i), (y+j), (z+k) } );
+					chunks.add( new Integer[] { (x+i), (y+j), (z-k) } );
+					chunks.add( new Integer[] { (x+i), (y-j), (z+k) } );
+					chunks.add( new Integer[] { (x+i), (y-j), (z-k) } );
+					chunks.add( new Integer[] { (x-i), (y+j), (z+k) } );
+					chunks.add( new Integer[] { (x-i), (y+j), (z-k) } );
+					chunks.add( new Integer[] { (x-i), (y-j), (z+k) } );
+					chunks.add( new Integer[] { (x-i), (y-j), (z-k) } );
 				}
 			}
 		}
 		
-		return chunkHolder;
+		return loadChunk();
 
 	}
 	
