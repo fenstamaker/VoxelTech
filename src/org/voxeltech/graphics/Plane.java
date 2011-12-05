5a63c686644a0f27a8a141753d41b4525ea9a744
fenstamaker <fenstamaker@gmail.com>
2011-12-05 13:29:43 -0500

    Trying to implement frustum culling. Failing to do so...

---
+++ src/org/voxeltech/graphics/Plane.java
@@ -0,0 +1,35 @@
+package org.voxeltech.graphics;
+
+import org.lwjgl.util.vector.*;
+
+public class Plane {
+
+	private Vector3f normal;
+	private Vector3f point;
+	private float D;
+	
+	public Plane(Vector3f p0, Vector3f p1, Vector3f p2) {
+		point = p0;
+		
+		Vector3f v = Vector3f.sub(p1, p0, null);
+		Vector3f u = Vector3f.sub(p2, p0, null);
+		
+		normal = Vector3f.cross(v, u, null);
+		normal.normalise();
+		D = -1 * Vector3f.dot(normal, point);
+	}
+	
+	public Plane(Vector3f _normal, Vector3f _point) {
+		normal = _normal;
+		point = _point;
+		normal.normalise();
+
+		D = -1 * Vector3f.dot(normal, point);
+	}
+	
+	public float getDistance(Vector3f p0) {
+		normal.normalise();
+		return Vector3f.dot(normal, p0) + D;
+	}
+	
+}
