eeef9235f65b43f6d60a4339122593c4b74ec4f2
Gary Fenstamaker <fenstamaker@gmail.com>
2013-08-16 01:36:52 -0400

    Commit

---
+++ src/MainWindow.java
@@ -0,0 +1,39 @@
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+
+import org.voxeltech.game.*;
+import org.voxeltech.graphics.*;
+
+/**
+ *
+ * @author Gary Fenstamaker
+ */
+public class MainWindow extends AbstractWindow {
+	
+    public Game game;
+    public WorldChunkHandler chunkHandler;
+    
+    public MainWindow(int _displayHeight, int _displayWidth) {
+    	super(_displayHeight, _displayWidth);
+    }
+
+    public static void main(String[] args) {
+    	MainWindow window = new MainWindow(600, 800);
+    	window.startDisplayLoop();
+    }
+    
+    public void startDisplayLoop() {
+    	Renderer.setup();
+    	game = new Game();
+        chunkHandler = new WorldChunkHandler();
+    	
+    	Thread thread2 = new Thread(chunkHandler);
+    	thread2.start();
+    	
+    	game.run();
+    	
+    	chunkHandler.keepRunning = false;
+    }
+
+}
